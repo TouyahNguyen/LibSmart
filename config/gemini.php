3a66783c14aa4034279b1fa15932dffd0263fbae
TouyahNguyen <khangcfa@gmail.com>
2025-08-22 15:41:58 +0700

    Update gemini.php

--- config/gemini.php
+++ config/gemini.php
@@ -2,6 +2,7 @@
 // config/gemini.php
 // Cấu hình Gemini API key
 return [
-    'api_key' => 'AIzaSyCJuYYeBy3aTTd0Rt3zaOrabT-2yFC4bOY', // Thay bằng API key thật của bạn
+    'api_key' => 'Your API Key', // Thay bằng API key thật của bạn
     'endpoint' => 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent',
 ];
+
